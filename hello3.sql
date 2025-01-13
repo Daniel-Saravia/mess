2b9575a3c067054e13fdea5f0cb0f37ac4c1caa5
Daniel Saravia <108732138+Daniel-Saravia@users.noreply.github.com>
2025-01-12 21:40:24 -0700

    Create hello3.sql

---
+++ hello3.sql
@@ -0,0 +1,23 @@
+-- Vendor Table
+SELECT * FROM vendor;
+
+-- Category Table
+SELECT * FROM category;
+
+-- Product Table
+SELECT * FROM product;
+
+-- Region Table
+SELECT * FROM region;
+
+-- Store Table
+SELECT * FROM store;
+
+-- Customer Table
+SELECT * FROM customer;
+
+-- SalesTransaction Table
+SELECT * FROM salestransaction;
+
+-- SoldVia Table
+SELECT * FROM soldvia;
